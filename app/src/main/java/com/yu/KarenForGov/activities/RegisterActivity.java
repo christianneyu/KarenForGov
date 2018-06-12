780dcbb7cd934620b4a189e5fb4f6e9c9fba07f5
ChristianneYu <yuchristianne@gmail.com>
2018-06-12 09:27:40 -0400

    UI for registration form and local database

---
+++ app/src/main/java/com/yu/KarenForGov/activities/RegisterActivity.java
@@ -0,0 +1,203 @@
+package com.yu.KarenForGov.activities;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.androidtutorialshub.KarenForGov.R;
+import com.yu.KarenForGov.helpers.InputValidation;
+import com.yu.KarenForGov.model.User;
+import com.yu.KarenForGov.sql.DatabaseHelper;
+
+import java.util.Calendar;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final AppCompatActivity activity = RegisterActivity.this;
+
+    private NestedScrollView nestedScrollView;
+
+    private TextInputLayout textInputLayoutName;
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+
+    private TextInputEditText textInputEditTextName;
+    private TextInputEditText textInputEditTextEmail;
+    private TextInputEditText textInputEditTextPassword;
+    private TextInputEditText textInputEditTextConfirmPassword;
+
+    private AppCompatButton appCompatButtonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    StringBuilder strBuf;
+    String birthdate;
+    private User user;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners();
+        initObjects();
+        this.showDatePickerDialog();
+    }
+
+    /**
+     * This method is to initialize views
+     */
+    private void initViews() {
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        textInputLayoutName = (TextInputLayout) findViewById(R.id.textInputLayoutName);
+        textInputLayoutEmail = (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+        textInputLayoutPassword = (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
+
+        textInputEditTextName = (TextInputEditText) findViewById(R.id.textInputEditTextName);
+        textInputEditTextEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
+        textInputEditTextPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+
+        appCompatButtonRegister = (AppCompatButton) findViewById(R.id.appCompatButtonRegister);
+
+        appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
+
+    }
+
+   private void initListeners() {
+        appCompatButtonRegister.setOnClickListener(this);
+        appCompatTextViewLoginLink.setOnClickListener(this);
+
+    }
+
+    private void initObjects() {
+        inputValidation = new InputValidation(activity);
+        databaseHelper = new DatabaseHelper(activity);
+        user = new User();
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.appCompatButtonRegister:
+                postDataToSQLite();
+                break;
+
+            case R.id.appCompatTextViewLoginLink:
+                finish();
+                break;
+        }
+    }
+
+    public void showDatePickerDialog() {
+        Button datePickerDialogButton = (Button) findViewById(R.id.datePickerDialogButton);
+        datePickerDialogButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+
+
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        strBuf = new StringBuilder();
+
+                        strBuf.append(year);
+                        strBuf.append("-");
+                        strBuf.append(monthOfYear + 1);
+                        strBuf.append("-");
+                        strBuf.append(dayOfMonth);
+                          Toast.makeText(RegisterActivity.this, "Birthday: " +strBuf.toString(),
+                                Toast.LENGTH_LONG).show();
+
+                        birthdate = strBuf.toString();
+
+
+                    }
+                };
+
+                // Get current year, month and day.
+                Calendar now = Calendar.getInstance();
+                int year = now.get(java.util.Calendar.YEAR);
+                int month = now.get(java.util.Calendar.MONTH);
+                int day = now.get(java.util.Calendar.DAY_OF_MONTH);
+
+                // Create the new DatePickerDialog instance.
+                DatePickerDialog datePickerDialog;
+                datePickerDialog = new DatePickerDialog(RegisterActivity.this, onDateSetListener, month, year, day);
+
+                // Set dialog icon and title.
+                datePickerDialog.setTitle("Please select date.");
+
+                // Popup the dialog.
+                datePickerDialog.show();
+            }
+        });
+    }
+    /**
+     * This method is to validate the input text fields and post data to SQLite
+     */
+    private void postDataToSQLite() {
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextName, textInputLayoutName, getString(R.string.error_message_name))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextEmail(textInputEditTextEmail, textInputLayoutEmail, getString(R.string.error_message_email))) {
+            return;
+        }
+        if (!inputValidation.isInputEditTextFilled(textInputEditTextPassword, textInputLayoutPassword, getString(R.string.error_message_password))) {
+            return;
+        }
+        if (!databaseHelper.checkUser(textInputEditTextEmail.getText().toString().trim())) {
+
+            user.setName(textInputEditTextName.getText().toString().trim());
+            user.setEmail(textInputEditTextEmail.getText().toString().trim());
+            user.setInterest(textInputEditTextPassword.getText().toString().trim());
+
+            databaseHelper.addUser(user);
+            Snackbar.make(nestedScrollView, getString(R.string.success_message), Snackbar.LENGTH_LONG).show();
+            emptyInputEditText();
+            Intent intentRegister = new Intent(getApplicationContext(),UsersListActivity.class);
+            startActivity(intentRegister);
+
+
+        } else {
+            // Snack Bar to show error message that record already exists
+            Snackbar.make(nestedScrollView, getString(R.string.error_email_exists), Snackbar.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText() {
+        textInputEditTextName.setText(null);
+        textInputEditTextEmail.setText(null);
+        textInputEditTextPassword.setText(null);
+
+    }
+}
---
+++ app/src/main/java/com/yu/KarenForGov/adapters/UsersRecyclerAdapter.java
@@ -0,0 +1,66 @@
+package com.yu.KarenForGov.adapters;
+
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androidtutorialshub.KarenForGov.R;
+import com.yu.KarenForGov.model.User;
+
+import java.util.List;
+
+
+
+public class UsersRecyclerAdapter extends RecyclerView.Adapter<UsersRecyclerAdapter.UserViewHolder> {
+
+    private List<User> listUsers;
+
+    public UsersRecyclerAdapter(List<User> listUsers) {
+        this.listUsers = listUsers;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // inflating recycler item view
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_user_recycler, parent, false);
+
+        return new UserViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        holder.textViewName.setText(listUsers.get(position).getName());
+        holder.textViewEmail.setText(listUsers.get(position).getEmail());
+        holder.textViewInterest.setText(listUsers.get(position).getInterest());
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.v(UsersRecyclerAdapter.class.getSimpleName(),""+listUsers.size());
+        return listUsers.size();
+    }
+
+
+    /**
+     * ViewHolder class
+     */
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+
+        public AppCompatTextView textViewName;
+        public AppCompatTextView textViewEmail;
+        public AppCompatTextView textViewInterest;
+
+        public UserViewHolder(View view) {
+            super(view);
+            textViewName = (AppCompatTextView) view.findViewById(R.id.textViewName);
+            textViewEmail = (AppCompatTextView) view.findViewById(R.id.textViewEmail);
+            textViewInterest = (AppCompatTextView) view.findViewById(R.id.textViewInterest);
+        }
+    }
+
+
+}
